d46e0178f6e75789a0887830f99d987660359c4a
fabiokerber <65039728+fabiokerber@users.noreply.github.com>
2020-08-25 15:09:17 -0300

    Add files via upload

--- roles/initial/templates/motd.sh
+++ roles/initial/templates/motd.sh
@@ -5,71 +5,71 @@ clear
 
 if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
         # Fall back to using the very slow lsb_release utility
-        DISTRIB_DESCRIPTION=$(lsb_release -s -d)
-fi
+	        DISTRIB_DESCRIPTION=$(lsb_release -s -d)
+		fi
 
-hostname_upper=$(echo $HOSTNAME | tr a-z A-Z)
+		hostname_upper=$(echo $HOSTNAME | tr a-z A-Z)
 
-echo "Servidor   : $hostname_upper"
-printf "Contato    : fabio.kerber@gmail.com"
-printf "\n"
-printf "LinkedIn    : https://www.linkedin.com/in/fabiokerber/"
-printf "\n"
-printf "\n"
-echo "Welcome to" $(echo $DISTRIB_DESCRIPTION | sed 's/\"//g') "kernel" $(uname -r)
-printf "\n"
+		echo "Server : $hostname_upper"
+		printf "\n"
+		echo "Welcome to" $(echo $DISTRIB_DESCRIPTION | sed 's/\"//g') "kernel" $(uname -r)
+		printf "\n"
 
-# Logged Users
-LoggedInUsers=$(who | wc -l)
+		# Logged Users
+		LoggedInUsers=$(who | wc -l)
 
-#System date
-date=`date`
+		#System date
+		date=`date`
 
-#System load
-LOAD1=`cat /proc/loadavg | awk {'print $1'}`
-LOAD5=`cat /proc/loadavg | awk {'print $2'}`
-LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+		#System load
+		LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+		LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+		LOAD15=`cat /proc/loadavg | awk {'print $3'}`
 
-#System uptime
-uptime=`cat /proc/uptime | cut -f1 -d.`
-upDays=$((uptime/60/60/24))
-upHours=$((uptime/60/60%24))
-upMins=$((uptime/60%60))
-upSecs=$((uptime%60))
+		#System uptime
+		uptime=`cat /proc/uptime | cut -f1 -d.`
+		upDays=$((uptime/60/60/24))
+		upHours=$((uptime/60/60%24))
+		upMins=$((uptime/60%60))
+		upSecs=$((uptime%60))
 
-#Root fs info
-root_free=`df -h / | awk '/\// {print $4}'|grep -v "^$"`
-boot_free=`df -h /boot | awk '/\// {print $4}'|grep -v "^$"`
+		#Root fs info
+		root_free=`df -h / | awk '/\// {print $4}'|grep -v "^$"`
+		boot_free=`df -h /boot | awk '/\// {print $4}'|grep -v "^$"`
+                var_free=`df -h /var | awk '/\// {print $4}'|grep -v "^$"`
+		#backup_free=`df -h /backup | awk '/\// {print $4}'|grep -v "^$"`
 
-#Memory Usage
-memory_usage=`free -m | grep Mem | awk '{print $3/$2 * 100.0}' | xargs printf "%.*f\n" 2 | sed 's/$/%/'`
+		#Memory Usage
+		memory_usage=`free -m | grep Mem | awk '{print $3/$2 * 100.0}' | xargs printf "%.*f\n" 2 | sed 's/$/%/'`
+		swap_usage=`free -m | grep Swap | awk '{print $3/$2 * 100.0}' | xargs printf "%.*f\n" 2 | sed 's/$/%/'`
 
-#Users
-users=`users | wc -w`
-USER=`whoami`
+		#Users
+		users=`users | wc -w`
+		USER=`whoami`
 
-#Processes
-processes=`ps aux | wc -l`
+		#Processes
+		processes=`ps aux | wc -l`
 
-#Interfaces
-INTERFACE=$(ip -4 ad | grep 'state UP' | awk -F ":" '!/^[0-9]*: ?lo/ {print $2}')
+		#Interfaces
+		INTERFACE=$(ip -4 ad | grep 'state UP' | awk -F ":" '!/^[0-9]*: ?lo/ {print $2}')
 
-echo "System information as of: $date"
-echo
-printf "System Load:\t%s %s %s\tSystem Uptime:\t\t%s "days" %s "hours" %s "min" %s "sec"\n" $LOAD1, $LOAD5, $LOAD15 $upDays $upHours $upMins $upSecs
-printf "Memory Usage:\t%s\t\t\tSwap Usage:\t\t%s\n" $memory_usage $swap_usage
-printf "Free On /:\t%s\t\t\tFree on /boot:\t\t%s\n" $root_free $boot_free
-printf "LoggedIn Users:\t%s\t\t\tWhoami:\t\t\t%s\n" $LoggedInUsers $USER
-printf "Processes:\t%s\t\t\t\n" $processes
-printf "\n"
-printf "Interface\tMAC Address\t\tIP Address\t\n"
+		echo "System information as of: $date"
+		echo
+		printf "System Load:\t%s %s %s\tSystem Uptime:\t\t%s "days" %s "hours" %s "min" %s "sec"\n" $LOAD1, $LOAD5, $LOAD15 $upDays $upHours $upMins $upSecs
+		printf "Memory Usage:\t%s\t\t\tSwap Usage:\t\t%s\n" $memory_usage $swap_usage
+		printf "Free On /:\t%s\t\t\tFree on /boot:\t\t%s\n" $root_free $boot_free
+		printf "Free On /var:\t%s\t\t\t" $var_free
+		printf "LoggedIn Users:\t%s\t\t\tWhoami:\t\t\t%s\n" $LoggedInUsers $USER
+		printf "Processes:\t%s\t\t\t\n" $processes
+		printf "\n"
+		printf "Interface\tMAC Address\t\tIP Address\t\n"
 
-for x in $INTERFACE
-  do
-        MAC=$(ip ad show dev $x |grep link/ether |awk '{print $2}')
-        IP=$(ip ad show dev $x |grep -v inet6 | grep inet|awk '{print $2}')
-        printf  $x"\t\t"$MAC"\t"$IP"\t\n"
+		for x in $INTERFACE
+		  do
+		          MAC=$(ip ad show dev $x |grep link/ether |awk '{print $2}')
+			          IP=$(ip ad show dev $x |grep -v inet6 | grep inet|awk '{print $2}')
+				          printf  $x"\t\t"$MAC"\t"$IP"\t\n"
 
-  done
+	    done
 echo
 echo
